fc6011607f83aa98877377b91cc04b1701b2656d
shack1234 <shadrack.anayo@strathmore.edu>
2020-05-05 15:55:11 +0300

    initial

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shadrack:1234@localhost/pitch'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
